4fec199191ecf6dd6b0b968904176144f6c86b62
LuisVvS <luluisv7@gmail.com>
2025-03-13 22:52:29 -0300

    Added ContaBancaria Class e the class of Test

--- src/main/java/com/br/ContaBancaria.java
+++ src/main/java/com/br/ContaBancaria.java
@@ -1,5 +1,36 @@
 package com.br;
 
 public class ContaBancaria {
-    
+    private double saldo;
+
+    public ContaBancaria(double saldo){
+        this.saldo = saldo;
+    }
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > saldo) {
+            return false;
+        } else {
+            saldo -= valor;
+            return true;
+        }
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
 }
